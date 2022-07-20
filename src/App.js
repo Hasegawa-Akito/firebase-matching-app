3d2b3668018be50070abace3ef967a8bcf43905f
Hasegawa-Akito <h.akito1999@gmail.com>
2022-07-21 00:23:30 +0900

    ユーザー名設定画面作成

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
-import { signInWithPopup } from 'firebase/auth';
 import './App.css';
-import { auth, provider } from './firebase';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useEffect, useState } from 'react';
+import SignIn from './component/SignIn';
 
 function App() {
-  const [user] = useAuthState(auth)
-  const [result, changeResult] = useState((<h3>ログイン前</h3>));
-  
-  useEffect(() => {
-    
-    if (user != null) {
-      changeResult((<h3>ログイン後</h3>));
-    }
-
-  },[user]);
   
   return (
     <div className="App">
-      {result}
-      <SignInButton />
+      
+      <SignIn />
     </div>
   );
 }
 
-function SignInButton() {
-
-  const signInGoogle = () => {
-    signInWithPopup(auth, provider);
-  }
-
-  return (
-    <button type="button" className="btn btn-primary" onClick={signInGoogle}>googleでログイン</button>
-  );
-}
 
 
 
---
+++ src/component/SignIn.js
@@ -0,0 +1,80 @@
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useEffect, useRef, useState } from 'react';
+import "../css/SignIn.css";
+
+function SignIn() {
+  const [user] = useAuthState(auth)
+  const [authResult, changeAuthResult] = useState((<SignInButton />));
+  
+  useEffect(() => {
+    //認証済みならユーザー名登録の画面を表示
+    if (user != null) {
+        changeAuthResult((<RegisterName />));
+    }
+  },[user]);
+  
+  return (
+    <div class="container">
+        <div class="login-container">
+                <div id="output"></div>
+                <div class="avatar"></div>
+                <div class="form-box">
+                    { authResult }
+                </div>
+            </div>
+            
+    </div>
+    
+  
+  );
+}
+
+function SignInButton() {
+  
+  // googleのログイン認証popupを表示
+  const signInGoogle = () => {
+    signInWithPopup(auth, provider);
+  }
+
+  return (
+    <div>
+        <h3 class="text-muted">ログイン認証</h3>
+        <button type="button" className="btn btn-dark login">Githubでログイン</button>
+        <button type="button" className="btn btn-primary login" onClick={signInGoogle}>googleでログイン</button>
+    </div>
+  );
+}
+
+function RegisterName() {
+    //inputの値を参照
+    const inputUserName = useRef("");
+
+    const [errorCheck, setErrorCheck] = useState(false);
+
+    // ユーザー名登録処理
+    const registerUser = () => {
+        const userName = inputUserName.current.value;
+
+        // ユーザー名が空文字の時
+        if (!userName) {
+            setErrorCheck(true);
+        }
+        else {
+            console.log(userName);
+        }
+    }
+
+    return (
+        <div>
+            {errorCheck ? <p class="text-danger">入力してください。</p> : <div class="hoge"></div>}
+            <input name="user" type="text" ref={ inputUserName } placeholder="ユーザーネーム" />
+            <button class="btn btn-primary login" type="submit" onClick={registerUser}>登録</button>
+        </div>
+        
+    );
+}
+
+
+export default SignIn;
